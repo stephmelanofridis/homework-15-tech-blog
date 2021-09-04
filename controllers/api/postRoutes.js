b4bfa95f7fb07c84888645911465e4d659e6cb22
Steph Melanofridis <stephmelanofridis@bigpond.com>
2021-09-04 15:01:27 +1000

    Added routes

---
+++ controllers/api/postRoutes.js
@@ -0,0 +1,52 @@
+const router = require('express').Router();
+const { Post } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.post('/', withAuth, async (req, res) => {
+    try {
+        const newPost = await Post.create({
+            ...req.body,
+            user_id: req.session.user_id,
+        });
+
+        res.status(200).json(newPost);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.put('/:id', withAuth, async (req, res) => {
+    try {
+        const postData = await Post.findByPk(req.params.id);
+        postData.update({
+            title: req.body.title,
+            content: req.body.content,
+            user_id: req.session.user_id
+        });
+
+        res.status(200).json(postData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+        const postData = await Post.destroy({
+            where: {
+                post_id: postData.id,
+            },
+        });
+
+        if (!postData) {
+            res.status(404).json({ message: 'No post found with this id!' });
+            return;
+        }
+
+        res.status(200).json(postData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
--- models/index.js
+++ models/index.js
@@ -3,19 +3,28 @@ const Post = require('./Post');
 const Comment = require('./Comment');
 
 User.hasMany(Post, {
-
+    foreignKey: 'user_id',
+    as: 'post'
 });
 
 Post.belongsTo(User, {
-
+    foreignKey: 'user_id',
+    as: 'content'
 });
 
 Post.hasMany(Comment, {
-
+    foreignKey: 'id',
+    as: 'comment'
 });
 
 Comment.belongsTo(Post, {
+    foreignKey: 'user_id',
+    as: 'post comment'
+});
 
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+    as: 'commentor'
 });
 
 module.exports = { User, Post, Comment }
