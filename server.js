617e0e27343833b68cc162206a9e6f23a3d43a7a
Steph Melanofridis <stephmelanofridis@bigpond.com>
2021-09-06 12:23:46 +1000

    Added public style sheet connection

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-// const controllers = require('./controllers');
+const controllers = require('./controllers');
 const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
@@ -10,8 +10,6 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-// const hbs = exphbs.create({ helpers });
 
 const sess = {
     secret: 'Super secret secret',
