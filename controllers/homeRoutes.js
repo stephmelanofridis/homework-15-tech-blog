0d6d7ca1698e86eff79994c79e3c79c0243c998e
Steph Melanofridis <stephmelanofridis@bigpond.com>
2021-09-06 15:33:51 +1000

    Adding routes to homeRoutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -20,4 +20,26 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.get('/post/:id', async (req, res) => {
+    try {
+        const postData = await Post.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    attributes: ['name'],
+                },
+            ],
+        });
+
+        const post = postData.get({ plain: true });
+
+        res.render('post', {
+            ...post,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 module.exports = router;
